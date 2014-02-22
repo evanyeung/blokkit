95529283245318c224d13513a45652e82a2755ec
Evan Yeung <evan.yeung1@gmail.com>
2014-02-22 15:13:59 -0500

    firebase search in progress

--- js/blokkit.js
+++ js/blokkit.js
@@ -3,9 +3,11 @@ var app = angular.module('blokkit', ['firebase']);
 function main($scope, $firebase) {
 	var fbActivities = new Firebase('https://blokkit.firebaseio.com/activities');
 	$scope.bloks = $firebase(fbActivities);
+	//console.log(Object.keys($scope.bloks.$child('act1')));
+	//console.log($scope.bloks.$child('act1').$child('details'));
 
-	// $scope.activities.$on('change', function() {
-	// 	console.log($scope.activities.act1.name);
-	// 	$scope.act1 = $scope.activities.act1.name;
-	// });
+	$scope.bloks.$on('loaded', function(snapshot) {
+		console.log(snapshot);
+		$scope.thing = snapshot.act1.name;
+	});
 }
