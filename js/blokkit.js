f7135bf278f9301743f776bc59a0e27cf270a00a
Evan Yeung <evan.yeung1@gmail.com>
2014-02-22 15:49:44 -0500

    search is working

--- js/blokkit.js
+++ js/blokkit.js
@@ -2,12 +2,20 @@ var app = angular.module('blokkit', ['firebase']);
 
 function main($scope, $firebase) {
 	var fbActivities = new Firebase('https://blokkit.firebaseio.com/activities');
-	$scope.bloks = $firebase(fbActivities);
+	$scope.bloksObj = $firebase(fbActivities);
 	//console.log(Object.keys($scope.bloks.$child('act1')));
 	//console.log($scope.bloks.$child('act1').$child('details'));
 
-	$scope.bloks.$on('loaded', function(snapshot) {
-		console.log(snapshot);
-		$scope.thing = snapshot.act1.name;
-	});
+	$scope.bloksObj.$on('loaded', function(snapshot) {
+	 	console.log(snapshot);
+	 	//$scope.thing = snapshot.act1.name;
+	 	var bloks = []
+	 	angular.forEach(snapshot, function(value, key){
+	 		console.log(value);
+	 		console.log(key);
+	 		bloks.push(value);
+	 	});
+	 	console.log(bloks);
+	 	$scope.bloks = bloks;
+	 });
 }
