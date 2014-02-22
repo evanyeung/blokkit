24f04b8297e73b445a9e7ecb24bdbcf79f600393
Evan Yeung <evan.yeung1@gmail.com>
2014-02-22 14:35:08 -0500

    can get data from firebase

--- js/blokkit.js
+++ js/blokkit.js
@@ -1,5 +1,11 @@
 var app = angular.module('blokkit', ['firebase']);
 
 function main($scope, $firebase) {
-	var root = new Firebase('https://blokkit.firebaseio.com/');
+	var fbActivities = new Firebase('https://blokkit.firebaseio.com/activities');
+	$scope.bloks = $firebase(fbActivities);
+
+	// $scope.activities.$on('change', function() {
+	// 	console.log($scope.activities.act1.name);
+	// 	$scope.act1 = $scope.activities.act1.name;
+	// });
 }
