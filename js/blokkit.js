f67d66160b7ec6234b273058678d89ff6afd88c8
Angela Chao <ajchao@uwaterloo.ca>
2014-02-23 05:59:20 -0500

    Like and hover

--- js/blokkit.js
+++ js/blokkit.js
@@ -21,31 +21,42 @@ app.factory('DB', function($firebase){
 	return {'activities': activities}
 });
 
-app.directive('gallery', function() {
+app.directive('tab', function(){
+	// Runs during compile
 	return {
-		restrict: 'A',
 		scope: {
-			activities: '='
+			cat: '=',
 		},
-		templateUrl: 'bloks.html'
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			element.on('click', function() {
+				console.log(scope.cat.cat);
+				scope.cat.cat = attrs.thisCat;
+				console.log(attrs.thisCat);
+			});
+		}
 	};
 });
 
 function main($scope, $firebase, ShowAddForm, DB) {
 
 	$scope.showAddForm = ShowAddForm;
+	$scope.bloks = [];
+	$scope.category = {'cat': 'all'};
 
-	$scope.bloks = DB.activities;
-
-	//$scope.bloks.$on('loaded', function(snapshot) {
-
-	 	// var bloks = []
-	 	// angular.forEach(snapshot, function(value, key){
-	 	// 	bloks.push(value);
-	 	// });
-	 	//$scope.activities.$bind('$scope', bloks);
-	 	//$scope.bloks = bloks;
-	 //});
+	//$scope.bloks = DB.activities;
+	var fbActivities = new Firebase('https://blokkit.firebaseio.com/activities');
+	
+	fbActivities.on('value', function(snapshot) {
+		if(snapshot.val() == null) {
+			alert("not existing");
+		} 
+		else {
+		 	angular.forEach(snapshot.val(), function(value, key){
+		 		$scope.bloks.push(value);
+		 	});
+		}
+	});
 }
 
 function form($scope, $firebase, ShowAddForm, DB) {
@@ -56,6 +67,21 @@ function form($scope, $firebase, ShowAddForm, DB) {
 		$scope.activities = DB.activities;
 
 		//var name = $scope.activity.verbose_name.toLowerCase().replace(' ', '_')
+		$scope.activity.categories = "";
+		if ($scope.activity.category1) {$scope.activity.categories = $scope.activity.categories + "sports "}
+		if ($scope.activity.category2) {$scope.activity.categories = $scope.activity.categories + "design "}
+		if ($scope.activity.category3) {$scope.activity.categories = $scope.activity.categories + "coding "}
+		if ($scope.activity.category4) {$scope.activity.categories = $scope.activity.categories + "games "}
+		if ($scope.activity.category5) {$scope.activity.categories = $scope.activity.categories + "tv "}
+		if ($scope.activity.category6) {$scope.activity.categories = $scope.activity.categories + "books "}
+		if ($scope.activity.category7) {$scope.activity.categories = $scope.activity.categories + "movies "}
+		
+		var now = Date.now();
+
+		$scope.activity.updated_at = now;
+		$scope.activity.$priority = now;
+		$scope.activity.likes = 0;
+
 		$scope.activities.$add({'activity': $scope.activity})
 
 		$scope.activityForm.$setPristine();
